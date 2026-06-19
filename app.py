9ab59c0e2368c001efca308154a04c54900f895d
feliceeeee <feliceyang88@gmail.com>
2026-06-19 14:28:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-model = joblib.load("artifacts/best_model.pkl")
+model = joblib.load("best_model.pkl")
 
 def main():
     st.title("Credit Score Prediction")
